2167ebc58c8389133300a63dffdceab12179ee0e
DwarfPorter <mrvlmor@gmail.com>
2020-05-25 22:42:07 +0300

    Retrofit

--- retrofit/app/src/main/java/ru/geekbrains/retrofit/MyApplication.java
+++ retrofit/app/src/main/java/ru/geekbrains/retrofit/MyApplication.java
@@ -2,6 +2,9 @@ package ru.geekbrains.retrofit;
 
 import android.app.Application;
 
+import okhttp3.OkHttpClient;
+import okhttp3.internal.cache.CacheInterceptor;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -10,9 +13,14 @@ public class MyApplication extends Application {
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(new LoggingInterceptor())
+                    .build();
+
             retrofit = new retrofit2.Retrofit.Builder()
                     .baseUrl("https://api.openweathermap.org/data/2.5/")
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
                     .build();
         }
         return retrofit;
